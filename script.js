56521ce463bb7becc8d0b09e4773a97bc997ca0a
Ishfaq Majeed Ahanger <ishfaaaaaaq@gmail.com>
2026-05-17 19:20:06 +0530

    Implement side menu toggle and light mode switch
    Added functionality for menu toggle and light mode.

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-const button = document.getElementById("theme-button");
+const menuIcon = document.getElementById("menuIcon");
+
+const sideMenu = document.getElementById("sideMenu");
+
+const moon = document.getElementById("moon");
+
+/* OPEN MENU */
+
+menuIcon.onclick = function () {
+
+    if (sideMenu.style.left === "0px") {
+
+        sideMenu.style.left = "-250px";
+
+    }
+
+    else {
+
+        sideMenu.style.left = "0px";
+
+    }
+
+};
+
+/* LIGHT MODE */
+
+moon.onclick = function () {
+
+    document.body.classList.toggle("light-mode");
+
+};const button = document.getElementById("theme-button");
 
 button.onclick = function () {
 
